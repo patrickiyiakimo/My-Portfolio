65e664b8ee6d63bdd0e60c11b573ebc1ab6a0cfd
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-06-20 19:31:32 +0100

    made new changes

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 const Portfolio = () => {
-  return (
-    <div>
-          <div>
-              
-        </div>
-    </div>
-  )
-}
+  const [details, setDetails] = React.useState([]);
 
-export default Portfolio
+  useEffect(() => {
+    fetch("/details.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setDetails(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className="mb-10">
+      {details.map((detail) => {
+        return (
+          <div
+            className="card w-96 bg-base-100 shadow-2xl mb-10 flex ml-20 mt-10"
+            key={detail._id}
+          >
+            <figure className="px-10 pt-10">
+              <img
+                src={detail.image}
+                alt="project image"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title underline ">
+                <a href="/">{detail.title}</a>
+              </h2>
+              <p>{detail.body}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Portfolio;
