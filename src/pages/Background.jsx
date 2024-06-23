b0eedb700a6655fa03fb3c8674f9feb57c3830c3
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-06-23 15:35:50 +0100

    added responsiveness

--- src/pages/Background.jsx
+++ src/pages/Background.jsx
@@ -1,9 +1,298 @@
-import React from 'react'
+import React from "react";
 
 const Background = () => {
   return (
-    <div>Background</div>
-  )
-}
+    <div className="bg-gray-900 text-gray-400">
+      <div className=" pt-20 pb-10 text-center md:flex md:items-center md:justify-center gap-96">
+        <h4 className="text-2xl font-bold">Companies I have Worked With</h4>
+        <h4 className="hidden md:block md:text-2xl md:font-bold">Education</h4>
+      </div>
+      <div>
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical"></ul>
+      </div>
+      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+        <li>
+          <div className="timeline-middle text-green-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
 
-export default Background
+          <div className="timeline-start mb-10 md:text-end bg-gray-800 p-10 ml-10 rounded-lg hover:bg-gray-700 hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+            <time className="font-mono italic">03/2024 - Present</time>
+            <p className="font-mono italic">Frontend Engineer</p>
+            <p className="font-mono italic">Lagos, Nigeria</p>
+            <div className="text-lg font-black">DotCampus Africa</div>
+            The Apple Macintosh—later rebranded as the Macintosh 128K—is the
+            original Apple Macintosh personal computer. It played a pivotal role
+            in establishing desktop publishing as a general office function. The
+            motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were
+            housed in a beige case with integrated carrying handle; it came with
+            a keyboard and single-button mouse.
+          </div>
+          <hr />
+        </li>
+
+        <li>
+          <hr />
+          <div className="timeline-middle text-green-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="hidden md:block timeline-end mb-10 md:mr-10 bg-gray-800 p-10 rounded-lg hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+            <time className="font-mono italic">2018</time>
+            <div className="text-lg font-black">JCSS</div>
+            iMac is a family of all-in-one Mac desktop computers designed and
+            built by Apple Inc. It has been the primary part of Apple's consumer
+            desktop offerings since its debut in August 1998, and has evolved
+            through seven distinct forms
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle text-green-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end bg-gray-800 p-10 ml-10 rounded-lg hover:bg-gray-700 hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+            <time className="font-mono italic">04/2024 – 06/2024</time>
+            <p className="font-mono italic">Frontend Engineer</p>
+            <p className="font-mono italic">Mumbai, India</p>
+            <div className="text-lg font-black">Harsh Gupta</div>
+            iMac is a family of all-in-one Mac desktop computers designed and
+            built by Apple Inc. It has been the primary part of Apple's consumer
+            desktop offerings since its debut in August 1998, and has evolved
+            through seven distinct forms
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle text-green-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="hidden  md:block timeline-end mb-10 md:mr-10 bg-gray-800 p-10 rounded-lg hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+            <time className="font-mono italic">2021</time>
+            <p className="font-mono italic">Estate Management And Valuation</p>
+            <div className="text-lg font-black">
+              Federal Polytechnique Nekede Owerri
+            </div>
+            iPhone is a line of smartphones produced by Apple Inc. that use
+            Apple's own iOS mobile operating system. The first-generation iPhone
+            was announced by then-Apple CEO Steve Jobs on January 9, 2007. Since
+            then, Apple has annually released new iPhone models and iOS updates.
+            As of November 1, 2018, more than 2.2 billion iPhones had been sold.
+            As of 2022, the iPhone accounts for 15.6% of global smartphone
+            market share
+          </div>
+          <hr />
+        </li>
+        <li>
+          <div className="timeline-middle text-green-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end bg-gray-800 p-10 ml-10 rounded-lg hover:bg-gray-700 hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+            <time className="font-mono italic">11/2023 – 06/2024</time>
+            <p className="font-mono italic">Frontend Engineer</p>
+            <p className="font-mono italic">Ikeja Lagos, Nigeria</p>
+            <div className="text-lg font-black">FlaveConsole</div>
+            The iPod is a discontinued series of portable media players and
+            multi-purpose mobile devices designed and marketed by Apple Inc. The
+            first version was released on October 23, 2001, about 8+1⁄2 months
+            after the Macintosh version of iTunes was released. Apple sold an
+            estimated 450 million iPod products as of 2022. Apple discontinued
+            the iPod product line on May 10, 2022. At over 20 years, the iPod
+            brand is the oldest to be discontinued by Apple
+          </div>
+          <hr />
+        </li>
+        <li>
+          <div className="timeline-middle text-green-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end bg-gray-800 p-10 ml-10 rounded-lg hover:bg-gray-700 hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+            <time className="font-mono italic">08/2023 – 11/2024</time>
+            <p className="font-mono italic">Frontend Engineer</p>
+            <p className="font-mono italic">Owerri, Nigeria</p>
+            <div className="text-lg font-black">BCH Onboarding</div>
+            iPhone is a line of smartphones produced by Apple Inc. that use
+            Apple's own iOS mobile operating system. The first-generation iPhone
+            was announced by then-Apple CEO Steve Jobs on January 9, 2007. Since
+            then, Apple has annually released new iPhone models and iOS updates.
+            As of November 1, 2018, more than 2.2 billion iPhones had been sold.
+            As of 2022, the iPhone accounts for 15.6% of global smartphone
+            market share
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle text-green-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end bg-gray-800 p-10 ml-10 rounded-lg hover:bg-gray-700 hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+            <time className="font-mono italic">03/2022 – 12/2023</time>
+            <p className="font-mono italic">Frontend Engineer</p>
+            <p className="font-mono italic">Owerri, Nigeria</p>
+            <div className="text-lg font-black">Uptrust Team</div>
+            The Apple Watch is a line of smartwatches produced by Apple Inc. It
+            incorporates fitness tracking, health-oriented capabilities, and
+            wireless telecommunication, and integrates with iOS and other Apple
+            products and services
+          </div>
+        </li>
+      </ul>
+
+      <div className="text-center text-2xl font-bold p-10 md:hidden">
+        <p>Education</p>
+      </div>
+      <div className="md:hidden">
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+          <li>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />{" "}
+                <div className="hidden md:block timeline-end mb-10 md:mr-10 bg-gray-800 p-10 rounded-lg hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+                  <time className="font-mono italic">2018</time>
+                  <div className="text-lg font-black">JCSS</div>
+                  iMac is a family of all-in-one Mac desktop computers designed
+                  and built by Apple Inc. It has been the primary part of
+                  Apple's consumer desktop offerings since its debut in August
+                  1998, and has evolved through seven distinct forms
+                </div>
+              </svg>
+            </div>
+            <div className=" md:block timeline-end mb-10 md:mr-10 bg-gray-800 p-10 rounded-lg hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+              <time className="font-mono italic">2018</time>
+              <div className="text-lg font-black">JCSS</div>
+              iMac is a family of all-in-one Mac desktop computers designed and
+              built by Apple Inc. It has been the primary part of Apple's
+              consumer desktop offerings since its debut in August 1998, and has
+              evolved through seven distinct forms
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="  md:block timeline-end mb-10 md:mr-10 bg-gray-800 p-10 rounded-lg hover:text-green-300 transform transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:border-b-4 hover:border-green-300">
+              <time className="font-mono italic">2021</time>
+              <p className="font-mono italic">
+                Estate Management And Valuation
+              </p>
+              <div className="text-lg font-black">
+                Federal Polytechnique Nekede Owerri
+              </div>
+              iPhone is a line of smartphones produced by Apple Inc. that use
+              Apple's own iOS mobile operating system. The first-generation
+              iPhone was announced by then-Apple CEO Steve Jobs on January 9,
+              2007. Since then, Apple has annually released new iPhone models
+              and iOS updates. As of November 1, 2018, more than 2.2 billion
+              iPhones had been sold. As of 2022, the iPhone accounts for 15.6%
+              of global smartphone market share
+            </div>
+            <hr />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Background;
