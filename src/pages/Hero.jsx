a183fd5b3b7a785a5baf693175bcd7d8e11f6ee3
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-06-23 01:04:45 +0100

    added my stacks images to the hero page

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -98,119 +98,27 @@
 // export default Hero;
 
 
-
-
-
-
-
-
-// import React from "react";
-// import background from "../images/galaxy.webp";
-// import Navbar from "../components/Navbar";
-// import RightSection from "./RightSection";
-// import avatar from "../images/avataaars.png";
-// import { FaGithub } from "react-icons/fa6";
-// import { FaLinkedin } from "react-icons/fa";
-// import { PiSelectionBackgroundFill } from "react-icons/pi";
-// import { FaCode } from "react-icons/fa";
-// import { TypeAnimation } from "react-type-animation";
-
-// const Hero = () => {
-//   return (
-//     <div
-//       className="bg-cover h-screen bg-bottom w-full pt-16"
-//       style={{ backgroundImage: `url(${background})` }}
-//     >
-//       <div className="bg-gray-900 bg-opacity-30 backdrop-blur-lg p-10 shadow-lg text-gray-200 mx-auto max-w-6xl pt-10 pb-10 rounded-md">
-//         <div className="bg-gray-800 pt-6 pb-10 bg-opacity-30 backdrop-blur-lg p-10 rounded-lg shadow-lg">
-//           <div className="hidden md:flex justify-between mb-10">
-//             <Navbar />
-//             <RightSection />
-//           </div>
-
-//           <div className="text-center">
-//             <img src={avatar} alt="my avatar" className="w-40 mx-auto mb-4" />
-//             <h1 className="text-xl md:text-4xl font-bold mb-4">
-//               Hello, I am Patrick Iyiakimo
-//             </h1>
-
-//             <TypeAnimation
-//               sequence={[
-//                 "I am a Frontend Engineer",
-//                 2000,
-//                 "With 2 years of experience ",
-//                 2000,
-//                 "and I am passionate about problem solving.",
-//                 2000,
-//                 "You can click on any of the Icons to know more about me. Thank You :)",
-//                 2000,
-//               ]}
-//               wrapper="span"
-//               speed={50}
-//               style={{ fontSize: "1em", display: "inline-block" }}
-//               repeat={Infinity}
-//             />
-//           </div>
-
-//           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-center text-gray-300 pt-10">
-//             <div className="hover:text-green-500">
-//               <a
-//                 href="https://github.com/patrickiyiakimo"
-//                 className="flex justify-center items-center space-x-2"
-//               >
-//                 <FaGithub />
-//                 <span>https://github.com/patrickiyiakimo</span>
-//               </a>
-//             </div>
-//             <div className="hover:text-green-500">
-//               <a
-//                 href="https://linkedin.com/in/patrick-iyiakimo-6a15a226a"
-//                 className="flex justify-center items-center space-x-2"
-//               >
-//                 <FaLinkedin />
-//                 <span>https://linkedin.com/in/patrick-iyiakimo-6a15a226a</span>
-//               </a>
-//             </div>
-//             <div className="hover:text-green-500">
-//               <a
-//                 href="/"
-//                 className="flex justify-center items-center space-x-2"
-//               >
-//                 <PiSelectionBackgroundFill />
-//                 <span>My Background</span>
-//               </a>
-//             </div>
-//             <div className="hover:text-green-500">
-//               <a
-//                 href="/"
-//                 className="flex justify-center items-center space-x-2"
-//               >
-//                 <FaCode />
-//                 <span>Other Skills & Hobbies</span>
-//               </a>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
-
-
-
-
 import React from "react";
 import background from "../images/galaxy.webp";
 import Navbar from "../components/Navbar";
 import RightSection from "./RightSection";
 import avatar from "../images/avataaars.png";
-import { FaGithub } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
-import { PiSelectionBackgroundFill } from "react-icons/pi";
-import { FaCode } from "react-icons/fa";
+import html from '../images/HTML logo.png';
+import css from "../images/CSS logo.png";
+import javascript from "../images/JavaScript.png";
+import tailwind from "../images/Tailwind logo.png";
+import next from "../images/Next js logo.png";
+import chakra from "../images/ChakraUI logo.jpeg";
+import material from "../images/Material UI logo.png";
+import typescript from "../images/Typescript logo.png";
+import react from "../images/React.png";
+import vue from "../images/Vue js logo.png";
+import nodejs from "../images/Node jslogo.png";
+import expressjs from "../images/Expressjs logo.png";
+// import { FaGithub } from "react-icons/fa6";
+// import { FaLinkedin } from "react-icons/fa";
+// import { PiSelectionBackgroundFill } from "react-icons/pi";
+// import { FaCode } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
@@ -220,9 +128,15 @@ const Hero = () => {
       style={{ backgroundImage: `url(${background})` }}
     >
       <div className="bg-gray-900 bg-opacity-30 backdrop-blur-lg p-10 shadow-lg text-gray-200 mx-auto max-w-6xl h-full flex flex-col justify-center rounded-md">
-        <div className="bg-gray-800 bg-opacity-30 backdrop-blur-lg p-10 rounded-lg shadow-lg">
-          <div className="mr-14 md:flex justify-between mb-10">
+        {/* <div className="mr-20 md:flex justify-between mb-10">
+          <Navbar />
+          <RightSection />
+        </div> */}
+
+        <div className="-mt-20 md:mt-3 bg-gray-800 bg-opacity-30 backdrop-blur-lg p-10 rounded-lg shadow-lg">
+          <div className=" md:mr-14 md:flex  md:mb-10">
             <Navbar />
+
             <RightSection />
           </div>
 
@@ -250,8 +164,20 @@ const Hero = () => {
             />
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-center text-gray-300 pt-10">
-            <div className="hover:text-green-500">
+          <div className="grid grid-cols-4 md:flex md:items-center md:justify-center gap-4 text-center text-gray-300 pt-10">
+            <img src={html} alt="html logo" className="w-8" />
+            <img src={css} alt="html logo" className="w-8" />
+            <img src={javascript} alt="html logo" className="w-8" />
+            <img src={vue} alt="html logo" className="w-8" />
+            <img src={react} alt="html logo" className="w-8" />
+            <img src={tailwind} alt="html logo" className="w-8" />
+            <img src={next} alt="html logo" className="w-8" />
+            <img src={typescript} alt="html logo" className="w-8" />
+            <img src={chakra} alt="html logo" className="w-8" />
+            <img src={material} alt="html logo" className="w-8" />
+            <img src={nodejs} alt="html logo" className="w-8" />
+            <img src={expressjs} alt="html logo" className="w-8" />
+            {/* <div className="hover:text-green-500">
               <a
                 href="https://github.com/patrickiyiakimo"
                 className="flex justify-center items-center space-x-2"
@@ -259,8 +185,8 @@ const Hero = () => {
                 <FaGithub />
                 <span>https://github.com/patrickiyiakimo</span>
               </a>
-            </div>
-            <div className="hover:text-green-500">
+            </div> */}
+            {/* <div className="hover:text-green-500">
               <a
                 href="https://linkedin.com/in/patrick-iyiakimo-6a15a226a"
                 className="flex justify-center items-center space-x-2"
@@ -268,8 +194,8 @@ const Hero = () => {
                 <FaLinkedin />
                 <span>https://linkedin.com/in/patrick-iyiakimo-6a15a226a</span>
               </a>
-            </div>
-            <div className="hover:text-green-500">
+            </div> */}
+            {/* <div className="hover:text-green-500">
               <a
                 href="/"
                 className="flex justify-center items-center space-x-2"
@@ -277,8 +203,8 @@ const Hero = () => {
                 <PiSelectionBackgroundFill />
                 <span>My Background</span>
               </a>
-            </div>
-            <div className="hover:text-green-500">
+            </div> */}
+            {/* <div className="hover:text-green-500">
               <a
                 href="/"
                 className="flex justify-center items-center space-x-2"
@@ -286,7 +212,7 @@ const Hero = () => {
                 <FaCode />
                 <span>Other Skills & Hobbies</span>
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
