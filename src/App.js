057ea2ec0a7984bccc18b14695ec5e24adf4452a
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-07-17 19:43:06 +0100

    added a skeleton to the portfolio projects

--- src/App.js
+++ src/App.js
@@ -3,27 +3,31 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import Background from "./pages/Background";
 import Contact from "./pages/Contact";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App ">
-        <Switch>
-          <Route exact path="/">
-            <Hero />
-          </Route>
-          <Route path="/background">
-            <Background />
-          </Route>
-          <Route path="/portfolio">
-            <Portfolio />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <SkeletonTheme baseColor="#202020" highlightColor="#444">
+      <Router>
+        <div className="App ">
+          <Switch>
+            <Route exact path="/">
+              <Hero />
+            </Route>
+            <Route path="/background">
+              <Background />
+            </Route>
+            <Route path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </SkeletonTheme>
   );
 }
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,89 +1,147 @@
-// import React, { useEffect } from "react";
-// // import background from "../images/galaxy.webp";
+// import React, { useEffect, useState } from "react";
+// import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+// import "react-loading-skeleton/dist/skeleton.css";
 
 // const Portfolio = () => {
-//   const [details, setDetails] = React.useState([]);
+//   const [details, setDetails] = useState([]);
+//   const [loading, setLoading] = useState(true);
 
 //   useEffect(() => {
 //     fetch("/details.json")
 //       .then((res) => res.json())
 //       .then((data) => {
-//         setDetails(data);
-//         console.log(data);
+//         setTimeout(() => {
+//           setDetails(data);
+//           setLoading(false);
+//         }, 3000);
 //       });
 //   }, []);
 
+//   if (loading) {
+//     return (
+//       <div className="flex justify-center items-center h-screen bg-gray-900">
+//         {/* <p className="text-2xl text-white">Loading...</p> */}
+//         <p><Skeleton/></p>
+//       </div>
+//     );
+//   }
+
 //   return (
-//     <div className="pb-10 bg-black">
-//       {details.map((detail) => {
-//         return (
-//           <div
-//             className="card w-96 bg-base-100 shadow-2xl mb-10 flex ml-20 pt-10"
-//             key={detail._id}
-//           >
-//             <figure className="px-10 pt-10">
-//               <img
-//                 src={detail.image}
-//                 alt="project image"
-//                 className="rounded-xl"
-//               />
-//             </figure>
-//             <div className="card-body items-center text-center">
-//               <h2 className="card-title underline ">
-//                 <a href="/">{detail.title}</a>
-//               </h2>
-//               <p>{detail.body}</p>
+//     <div className="pb-10 bg-gray-900">
+//       <h1 className="text-center text-2xl font-bold pt-5">My Portfolio</h1>
+//       <div className="md:grid md:grid-cols-2 gap-10 px-20 pt-10">
+//           {details.map((detail) => (
+//             <div
+//               className="card bg-gray-800 shadow-2xl mb-10 pt-10 h-full text-gray-400"
+//               key={detail._id}
+//             >
+//               <figure className="px-10 pt-10">
+//                 {detail.image ? (
+//                   <img
+//                     src={detail.image}
+//                     alt="project image"
+//                     className="rounded-xl"
+//                   />
+//                 ) : (
+//                   <Skeleton height={200} width={300} />
+//                 )}
+//               </figure>
+//               <div className="hover:text-green-300 transform transition-transform duration-500 ease-in-out hover:-translate-y-6 hover:border-b-4 hover:border-green-300 card-body bg-gray-600 rounded-lg items-center text-center">
+//                 <h2 className="card-title underline">
+//                   <a href="/">{detail.title || <Skeleton />}</a>
+//                 </h2>
+//                 <p>{detail.body || <Skeleton count={4} />}</p>
+//               </div>
 //             </div>
-//           </div>
-//         );
-//       })}
+//           ))}
+//       </div>
 //     </div>
 //   );
 // };
 
 // export default Portfolio;
 
-import React, { useEffect } from "react";
-// import background from "../images/galaxy.webp";
+
+
+import React, { useEffect, useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Portfolio = () => {
-  const [details, setDetails] = React.useState([]);
+  const [details, setDetails] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("/details.json")
       .then((res) => res.json())
       .then((data) => {
-        setDetails(data);
-        console.log(data);
+        setTimeout(() => {
+          setDetails(data);
+          setLoading(false);
+        }, 3000);
       });
   }, []);
 
+  if (loading) {
+    return (
+      <SkeletonTheme baseColor="#808080" highlightColor="#444">
+        <div className="pb-10 bg-gray-900">
+          <h1 className="text-center text-2xl font-bold pt-5">
+            <Skeleton width={200} />
+          </h1>
+          <div className="md:grid md:grid-cols-2 gap-10 px-20 pt-10">
+            {[...Array(4)].map((_, index) => (
+              <div
+                className="card bg-gray-800 shadow-2xl mb-10 pt-10 h-full text-gray-400"
+                key={index}
+              >
+                <figure className="px-10 pt-10">
+                  <Skeleton height={50} width={300} />
+                </figure>
+                <div className="card-body bg-gray-600 rounded-lg items-center text-center">
+                  <h2 className="card-title underline">
+                    <Skeleton width={150} />
+                  </h2>
+                  <p>
+                    <Skeleton count={4} />
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </SkeletonTheme>
+    );
+  }
+
   return (
     <div className="pb-10 bg-gray-900">
-      <h1 className="text-center text-2xl font-bold  pt-5">My Portfolio</h1>
-      <div className="md:grid md:grid-cols-2 gap-10 px-20 pt-10 ">
-        {details.map((detail) => {
-          return (
-            <div
-              className="card bg-gray-800 shadow-2xl mb-10 pt-10 h-full text-gray-400"
-              key={detail._id}
-            >
-              <figure className="px-10 pt-10">
+      <h1 className="text-center text-2xl font-bold pt-5">My Portfolio</h1>
+      <div className="md:grid md:grid-cols-2 gap-10 px-20 pt-10">
+        {details.map((detail) => (
+          <div
+            className="card bg-gray-800 shadow-2xl mb-10 pt-10 h-full text-gray-400"
+            key={detail._id}
+          >
+            <figure className="px-10 pt-10">
+              {detail.image ? (
                 <img
                   src={detail.image}
                   alt="project image"
                   className="rounded-xl"
                 />
-              </figure>
-              <div className="hover:text-green-300 transform transition-transform duration-500 ease-in-out hover:-translate-y-6 hover:border-b-4 hover:border-green-300 card-body bg-gray-600 rounded-lg items-center text-center">
-                <h2 className="card-title underline">
-                  <a href="/">{detail.title}</a>
-                </h2>
-                <p>{detail.body}</p>
-              </div>
+              ) : (
+                <Skeleton height={200} width={300} />
+              )}
+            </figure>
+            <div className="hover:text-green-300 transform transition-transform duration-500 ease-in-out hover:-translate-y-6 hover:border-b-4 hover:border-green-300 card-body bg-gray-600 rounded-lg items-center text-center">
+              <h2 className="card-title underline">
+                <a href="/">{detail.title}</a>
+              </h2>
+              <p>{detail.body}</p>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
